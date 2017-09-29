2a4a4b6878e2a439f1464f69ed207736bd15692d
zhitang <Seven0711@users.noreply.github.com>
2017-09-29 14:57:51 +0800

    Update main.js
    字节过滤器

--- src/main.js
+++ src/main.js
@@ -4,6 +4,26 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 
+//filter须在Vue实例化前注册
+//字节转化
+Vue.filter('bytesToSize', (bytes) => {
+  if (bytes < 1) {
+    return '0 B';
+  }
+  let k = 1024,
+    sizes = ['B',
+      'KB',
+      'MB',
+      'GB',
+      'TB',
+      'PB',
+      'EB',
+      'ZB',
+      'YB'
+    ],
+    i = Math.floor(Math.log(bytes) / Math.log(k));
+  return (bytes / Math.pow(k, i)).toFixed(0) + ' ' + sizes[i];
+});
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
